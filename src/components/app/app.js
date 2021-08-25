f3391f0fb9d783dce75da22b6c79da4dad62d805
Dmitrii <dmitrii@openocean.academy>
2021-08-25 23:56:04 +0300

    added componentDidCatch

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,13 +4,16 @@ import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ItemList from '../item-list';
 import PersonDetails from '../person-details';
+import ErrorButton from '../error-button';
+import ErrorIndicator from '../error-indicator';
 
 import './app.css';
 
 export default class App extends React.Component {
 
   state = {
-    selectedPerson: null
+    selectedPerson: null,
+    hasError: false
   }
 
   onPersonSelected = (id) => {
@@ -19,19 +22,27 @@ export default class App extends React.Component {
     })
   }
 
+  componentDidCatch() {
+    this.setState({ hasError: true });
+  }
 
   render() {
+
+    if (this.state.hasError) {
+      return <ErrorIndicator />
+    }
     return (
       <div>
         <Header />
         <RandomPlanet />
-
+        <ErrorButton />
         <div className="row mb2">
           <div className="col-md-6">
             <ItemList onItemSelected={this.onPersonSelected} />
           </div>
           <div className="col-md-6">
             <PersonDetails personId={this.state.selectedPerson} />
+            
           </div>
         </div>
       </div>
