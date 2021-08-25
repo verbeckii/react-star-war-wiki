d8d476e37874e03297d5a9ba4e7a781eb063ee6f
Dmitrii <dmitrii@openocean.academy>
2021-08-25 23:39:04 +0300

    added setInterval

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -17,6 +17,11 @@ export default class RandomPlanet extends Component {
 
   componentDidMount() {
     this.updatePlanet();
+    this.interval = setInterval(this.updatePlanet, 5000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 
   onPlanetLoaded = (planet) => {
