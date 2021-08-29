9f1667f66d88b3c09876186a77cd59c5ddb779ff
Dmitrii <dmitrii@openocean.academy>
2021-08-29 23:42:54 +0300

    added ErrorBoundry component

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -6,28 +6,15 @@ import ErrorIndicator from '../error-indicator/error-indicator';
 
 import './people-page.css';
 import SwapiService from '../../services/swapi-services';
+import ErrorBoundry from '../error-boundry';
+import Row from '../row'
 
-
-const Row = ({ left, right }) => {
-  return (
-    <div className="row mb-2">
-      <div className="col-md-6">
-        {left}
-      </div>
-      <div className="col-md-6">
-        {right}
-      </div>
-    </div>
-  );
-};
-
-export default class PeoplePage extends React.Component {
+export default class PeoplePage extends Component {
 
   swapiService = new SwapiService();
 
   state = {
     selectedPerson: 3,
-    hasError: false
   };
 
   componentDidCatch(error, info) {
@@ -56,8 +43,10 @@ export default class PeoplePage extends React.Component {
     );
 
     const personDetails = (
-      <PersonDetails personId={this.state.selectedPerson} />
-    )
+      <ErrorBoundry>
+        <PersonDetails personId={this.state.selectedPerson} />
+      </ErrorBoundry>
+    );
 
     return (
       <Row left={itemList} right={personDetails} />
