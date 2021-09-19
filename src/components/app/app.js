d69ecdda0fcd1723646516e3e09a4b82b3ff55ac
Dmitrii Verbetsky <dmitrii@openocean.academy>
2021-09-19 23:40:43 +0300

    Revert "clone react element"
    This reverts commit 7e3f1c552937ae59762d491f366ca39bee1438d0.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import ErrorBoundry from '../error-boundry';
-
-import Row from "../row/row";
-import ItemDetails, { Record } from "../item-details/item-details";
-import SwapiService from "../../services/swapi-services";
+import ErrorIndicator from '../error-indicator';
+import PeoplePage from '../people-page';
 
 import './app.css';
 
-export default class App extends Component {
+import SwapiService from '../../services/swapi-services';
+import ItemList from "../item-list/item-list";
+import PersonDetails from "../person-details/person-details";
+
+export default class App extends React.Component {
 
   swapiService = new SwapiService();
 
   state = {
-    showRandomPlanet: true
+    showRandomPlanet: true,
+    hasError: false
   };
 
   toggleRandomPlanet = () => {
@@ -26,52 +28,54 @@ export default class App extends Component {
     });
   };
 
+  componentDidCatch() {
+    this.setState({ hasError: true });
+  }
+
   render() {
+
+    if (this.state.hasError) {
+      return <ErrorIndicator />
+    }
 
     const planet = this.state.showRandomPlanet ?
       <RandomPlanet/> :
       null;
 
-    const { getPerson,
-            getStarship,
-            getPersonImage,
-            getStarshipImage } = this.swapiService;
-
-    const personDetails = (
-      <ItemDetails
-        itemId={11}
-        getData={getPerson}
-        getImageUrl={getPersonImage} >
-
-        <Record field="gender" label="Gender" />
-        <Record field="eyeColor" label="Eye Color" />
-
-      </ItemDetails>
-    );
-
-    const starshipDetails = (
-      <ItemDetails
-        itemId={5}
-        getData={getStarship}
-        getImageUrl={getStarshipImage}>
-
-        <Record field="model" label="Model" />
-        <Record field="length" label="Length" />
-        <Record field="costInCredits" label="Cost" />
-      </ItemDetails>
-
-    );
-
     return (
-      <ErrorBoundry>
-        <div className="stardb-app">
-          <Header />
+      <div className="stardb-app">
+        <Header />
+        { planet }
 
-          <Row
-            left={personDetails}
-            right={starshipDetails} />
+        <PeoplePage />
+
+        <div className="row mb-2">
+          <div className="col-md-6">
+            <ItemList
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllPlanets}
+              renderItem={(item) => item.name}
+              />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
         </div>
-      </ErrorBoundry>
+
+        <div className="row mb-2">
+          <div className="col-md-6">
+            <ItemList
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllStarships}
+              renderItem={(item) => item.name}
+              />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
+        </div>
+
+      </div>
     );
   }
 }
